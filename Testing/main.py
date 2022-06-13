fb60469b42ff8bf9f9bb8a99f8637199c579076d
Miquel Gargallo Sieiro <4174255094@edu.gencat.cat>
2022-06-13 11:40:42 +0200

    Change graph generation

--- Testing/main.py
+++ Testing/main.py
@@ -5,7 +5,7 @@ import os.path
 import os
 import matplotlib.pyplot as plt
 from PIL import ImageTk, Image
-import prueba_file2
+import graph_generator
 
 def split(*args):
     ExitText.set(" ".join(str(InitText.get())))
@@ -13,7 +13,7 @@ def split(*args):
 
 def GenerarGrafo(*args):
     plt.cla()
-    prueba_file2.Grafo()
+    graph_generator.Grafo()
     print("Imagen puesta")
 
 
@@ -43,7 +43,7 @@ ExitLabel.grid(column = 1, row = 2, padx = 20, pady = 20)
 # Creem la direcció de la imatge que guardarem sempre a la carpeta de imatges situada en la carpeta pare de l'actual.
 
 my_file = "white.jpg"
-image_path = os.path.join(os.path.join(os.path.pardir,prueba_file2.PATH_IMAGES), my_file)
+image_path = os.path.join(os.path.join(os.path.pardir,graph_generator.PATH_IMAGES), my_file)
 Img = ImageTk.PhotoImage(Image.open(image_path))
 ImgLabel = ttk.Label(mainframe, image=Img)
 ImgLabel.grid(column=1, row=3, padx=10)
