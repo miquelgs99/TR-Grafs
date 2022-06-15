cc3d749f7d6a26b2487bd16edcb80a20503b0ac5
miquelgs99 <mgargallo@institutsantaeulalia.cat>
2022-06-15 18:49:33 +0200

    Reduce amount of edges in the graph

--- Testing/graph_generator.py
+++ Testing/graph_generator.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def grafo(*args):
@@ -7,8 +8,13 @@ def grafo(*args):
     matrix = np.random.randint(2, size=(size, size))
 
     for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] != matrix[j][i]:
+                matrix [i][j] = matrix[j][i]
         matrix[i][i] = 0
-    G = nx.from_numpy_matrix(matrix)
+    graph = nx.from_numpy_matrix(matrix)
 
-    nx.draw_random(G)
+    nx.draw_random(graph)
+    plt.show()
 
+grafo()
--- Testing/main.py
+++ Testing/main.py
@@ -45,9 +45,12 @@ def show_graph(frame, *args):
 
     matrix = np.random.randint(2, size=(size, size))
     for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] != matrix[j][i]:
+                matrix [i][j] = matrix[j][i]
         matrix[i][i] = 0
     graph = nx.from_numpy_matrix(matrix)
-
+    print(graph.number_of_edges())
     nx.draw_random(graph)
 
     fig, ax = plt.subplots()
