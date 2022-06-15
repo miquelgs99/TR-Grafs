c605fbcf1fb14b4a42001dce31b71b859e897ac7
Miquel Gargallo Sieiro <4174255094@edu.gencat.cat>
2022-06-15 11:11:54 +0200

    Change variable names

--- Testing/graph_generator.py
+++ Testing/graph_generator.py
@@ -5,12 +5,13 @@ import numpy as np
 import os
 from tkinter import *
 from tkinter import ttk
-PATH_IMAGES= "images"
+PATH_IMAGES = "images"
 
 def Grafo(*args):
 
-    MSize = 10
+    MSize = 15
     matrix = np.random.randint(2, size=(MSize, MSize))
+
     for i in range(len(matrix)):
         matrix[i][i] = 0
     G=nx.from_numpy_matrix(matrix)
@@ -18,12 +19,12 @@ def Grafo(*args):
     nx.draw_random(G)
 
     # Creem la direcció de la imatge que guardarem sempre a la carpeta de imatges situada en la carpeta pare de l'actual.
-    my_path = os.path.abspath(os.path.join(os.path.pardir,PATH_IMAGES))  # Figures out the absolute path for you in case your working directory moves around.
+    my_path = os.path.abspath(os.path.join(os.path.pardir,  PATH_IMAGES))  # Figures out the absolute path for you in case your working directory moves around.
 
     my_file = "figure.jpg"
 
     plt.savefig(os.path.join(my_path, my_file))
     print("Imagen creada")
-    plt.show(block = False)
+    plt.show(block=False)
 
 
