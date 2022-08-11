0f8ea702d755a701620595042877df48e975beb6
miquelgs99 <mgargallo@institutsantaeulalia.cat>
2022-08-11 11:47:08 +0200

    Node selection done

--- generator/main.py
+++ generator/main.py
@@ -22,17 +22,35 @@ def highlighter(event):
     graph = event.artist.graph
 
     # clear any non-default color on nodes
-    for node, attributes in graph.nodes.data():
-        attributes.pop('color', None)
+    for node, attributes in graph.nodes(data=True):
+        if len(node_picker) == 2:
+            attributes.pop('color', None)
+        else:
+            pass
 
-    for u, v, attributes in graph.edges.data():
-        attributes.pop('width', None)
+    for u, v, attributes in graph.edges(data=True):
+        if len(node_picker) == 2:
+            attributes.pop('width', None)
+        else:
+            pass
+
+    if len(node_picker) == 0:
+        node_picker.append(event.nodes[0])
+    elif len(node_picker) == 1:
+        if event.nodes[0] == node_picker[0]:
+            pass
+        else:
+            node_picker.append(event.nodes[0])
+    else:
+        node_picker.clear()
+        node_picker.append(event.nodes[0])
 
     for node in event.nodes:
-        graph.nodes[node]['color'] = 'C1'
+        graph.nodes[node]['color'] = 'red'
+        # for edge_attribute in graph[node].values():
+        #     edge_attribute['width'] = 3
 
-        for edge_attribute in graph[node].values():
-            edge_attribute['width'] = 3
+    picked_nodes.set(str(node_picker))
 
     # update the screen
     event.artist.stale = True
@@ -51,7 +69,7 @@ def show_graph(frame, *args):
         messagebox.showinfo(title="Error", message="Enter a valid number!")
 
     # The matrix of the graph is created with randint from numpy
-    matrix = np.random.randint(-30, 20, size=(size, size))
+    matrix = np.random.randint(-100, 20, size=(size, size))
 
     # We go through the matrix to make it symmetrical and also eliminate the self-connected edges
     for i in range(len(matrix)):
@@ -67,19 +85,32 @@ def show_graph(frame, *args):
     # We create the graph from the matrix
     graph = nx.from_numpy_matrix(matrix)
 
+    for node, node_attr in graph.nodes(data=True):
+        node_attr['size'] = 500
+
     # ORIGINAL ------------------
 
     # # We create the figures where the graph will be
+
+    # def create_random_layout(*args):
+    #     pos = nx.random_layout(graph, seed=4583345)
+    #     return pos
+
     fig, ax = plt.subplots()
     pprint.pprint(matrix)
-    art = plot_network(graph, layout="shell", ax=ax, node_style=use_attributes(),
-                       edge_style=use_attributes())
+    art = plot_network(graph, layout="shell", ax=ax,
+                       node_style=use_attributes(),
+                       edge_style=use_attributes(),
+                       node_label_style={'font_size': 7,
+                                         'font_weight': 'bold',
+                                         'font_color': 'k',
+                                         'bbox': {'alpha': 0}
+                                         })
 
     # Afegim per sobre el graf etiquetat de forma no interactiva.
     edge_labels = nx.get_edge_attributes(graph, "weight")
     pos = nx.shell_layout(graph)
-    nx.draw_networkx_edges(graph, pos)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels)
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels, font_size=8)
 
     # # I don't know what this does
     art.set_picker(10)
@@ -92,13 +123,18 @@ def show_graph(frame, *args):
     canvas.get_tk_widget().grid(column=0, row=1, sticky=(N, W, E, S), padx=20)
 
 
-# We create the main window and we define some characteristics
+# Declaring variables
+node_picker = []
+
+# region We create the main window, and we define some characteristics
 root = Tk()
 root.title("TR-Grafs")
 root.geometry('1280x720')
 root.columnconfigure(0, weight=2)   # We define the grid that the window will have
 root.rowconfigure(0, weight=2)      # We define the grid that the window will have
+# endregion
 
+# region Creating the frames
 # We create the frame where the text will be shown
 text_frame = ttk.Frame(root)
 text_frame.grid(column=0, row=0, sticky=W)
@@ -106,14 +142,30 @@ text_frame.grid(column=0, row=0, sticky=W)
 # We create the frame where the graphs will be shown
 graph_frame = ttk.Frame(root)
 graph_frame.grid(column=0, row=0, sticky=E)
+# endregion
+
+# region Creating the labels
 
 # We put some text explaining what to write in the text box
 vertex_label = ttk.Label(text_frame, text="Introduce the number of vertex that you want: ")
 vertex_label.grid(column=0, row=0, padx=20, pady=20)
 
-# We create the entry text box
+# We create the label where the picked nodes will be
+picked_nodes = StringVar()    # The string variable where the label is associated
+
+picked_nodes_label = ttk.Label(text_frame, textvariable=picked_nodes)
+picked_nodes_label.grid(column=2, row=2, padx=20, pady=20)
+
+# endregion
+
+# region We create the entry text box
+
 vertex_entry = ttk.Entry(text_frame, width=15)
 vertex_entry.grid(column=1, row=0, padx=0, pady=20)
+
+# endregion
+
+# region Creating the buttons
 
 # We create the button that will generate the graph, and we assign the previous function made to it
 generate_button = ttk.Button(text_frame, text="Generate!", command=lambda: show_graph(graph_frame))
@@ -121,13 +173,16 @@ generate_button.grid(column=1, row=1)
 
 # We create the button that will create the random number, and we assign the previous function made to it
 random_button = ttk.Button(text_frame, text="Random number!",
-                           command=lambda: [vertex_entry.delete(0, END),  # deletes the current value
-                                            vertex_entry.insert(0, np.random.randint(3, 20))])  # inserts new value assigned by 2nd parameter
+                           command=lambda: [vertex_entry.delete(0, END),  # Deletes the current value
+                                            vertex_entry.insert(0, np.random.randint(3, 20))  # Inserts new value
+                                            ])
 random_button.grid(column=1, row=2)
-# random_number(vertex_entry)
+
 # We create a button that shut down the program
 quit_button = ttk.Button(text_frame, text="Quit!", command=exit)  # The function exit closes the code directly
 quit_button.grid(column=1, row=3, pady=10)
+
+# endregion
 
 
 vertex_entry.focus()       # We focus the text writing in the text box when initializing the code
