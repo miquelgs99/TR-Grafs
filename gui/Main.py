bbb90cccca9639f4bbbfb714ff106bb72cebd3eb
Jeroni <jeronicarandell@gmail.com>
2022-08-17 18:37:28 +0200

    Turn gui main into a Class

--- gui/Main.py
+++ gui/Main.py
@@ -3,8 +3,21 @@ from tkinter import ttk
 import MenuFrame
 
 
-root = Tk()
-root.geometry('1280x720')
-main_win = MenuFrame.MenuFrame()
-main_win.grid(row=0, column=0, padx=10, pady=10)
-root.mainloop()
+class MainGui:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry('1280x720')
+        main_win = MenuFrame.MenuFrame()
+        main_win.grid(row=0, column=0, padx=10, pady=10)
+
+    def run_gui(self):
+        self.root.mainloop()
+
+    def refresh(self):
+        self.destroy()
+        self.__init__()
+
+
+if __name__ == '__main__':
+    mainGui = MainGui()
+    mainGui.run_gui()
