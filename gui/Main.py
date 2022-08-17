35dfd0a5462ca353842c08432b3c374de2655f26
Jeroni <jeronicarandell@gmail.com>
2022-08-17 18:55:22 +0200

    Remove root from Main Gui.

--- gui/Main.py
+++ gui/Main.py
@@ -4,14 +4,13 @@ import MenuFrame
 
 
 class MainGui:
+
     def __init__(self):
-        self.root = Tk()
-        self.root.geometry('1280x720')
-        main_win = MenuFrame.MenuFrame()
-        main_win.grid(row=0, column=0, padx=10, pady=10)
+        self.main_win = MenuFrame.MenuFrame()
+        self.main_win.grid(row=0, column=0, padx=10, pady=10)
 
     def run_gui(self):
-        self.root.mainloop()
+        self.main_win.mainloop()
 
     def refresh(self):
         self.destroy()
