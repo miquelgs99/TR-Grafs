0e597c53f16135b2065c5df63a4d9bb6c6b4b4b8
miquelgs99 <mgargallo@institutsantaeulalia.cat>
2022-09-10 19:53:13 +0200

    Change between coloring and sudoku by clicking on it

--- gui/SudokuColoringFrame.py
+++ gui/SudokuColoringFrame.py
@@ -42,24 +42,23 @@ class SudokuColoringFrame(Main.StdFrame):
 
         self.which_frame = "Coloring"
 
-        self.right_label = ttk.Label(top_text_frame, text='Coloring',
-                                     foreground='#F0F0F0', font=('Segoe UI', 15))
-        self.right_label.grid(column=0, row=0, padx=20)
+        self.left_label = ttk.Label(top_text_frame, text='Coloring', foreground='#F0F0F0', font=('Segoe UI', 15))
+        self.left_label.grid(column=0, row=0, padx=20)
 
         self.mid_label = ttk.Label(top_text_frame, text='Coloring', font=('Segoe UI', 20))
         self.mid_label.grid(column=1, row=0, padx=20)
 
-        self.left_label = ttk.Label(top_text_frame, text='Sudoku', font=('Segoe UI', 15))
-        self.left_label.grid(column=2, row=0, sticky=EW, padx=20)
+        self.right_label = ttk.Label(top_text_frame, text='Sudoku', font=('Segoe UI', 15))
+        self.right_label.grid(column=2, row=0, sticky=EW, padx=20)
+
+        self.left_label.bind("<Button-1>", lambda event, btn="left": self.change_to(btn))
+        self.right_label.bind("<Button-1>", lambda event, btn="right": self.change_to(btn))
 
         # region Creating the buttons
 
         # We create the button that will generate the graph, and we assign the previous function made to it
         generate_button = ttk.Button(text_frame, text="Generate!", command=self.show_graph)
         generate_button.grid(column=1, row=1, padx=10, pady=10)
-
-        change_button = ttk.Button(text_frame, text="Change!", command=self.change_to)
-        change_button.grid(column=1, row=2, padx=10, pady=10)
 
         # We create the button that will create the random number, and we assign the previous function made to it
         random_button = ttk.Button(text_frame, text="Random number!",
@@ -84,18 +83,23 @@ class SudokuColoringFrame(Main.StdFrame):
 
         # endregion
 
-    def change_to(self):
+    def change_to(self, btn, event=None):
 
-        if self.which_frame == "Coloring":
-            self.right_label.configure(foreground='black')
-            self.mid_label.configure(text='Sudoku')
-            self.left_label.configure(foreground='#F0F0F0')
-            self.which_frame = "Sudoku"
-        else:
-            self.right_label.configure(foreground='#F0F0F0')
-            self.mid_label.configure(text='Coloring')
+        if self.which_frame == "Coloring" and btn == "right":
             self.left_label.configure(foreground='black')
+            self.mid_label.configure(text='Sudoku')
+            self.right_label.configure(foreground='#F0F0F0')
+            self.which_frame = "Sudoku"
+
+            # Put here the frame change
+
+        elif self.which_frame == "Sudoku" and btn == "left":
+            self.left_label.configure(foreground='#F0F0F0')
+            self.mid_label.configure(text='Coloring')
+            self.right_label.configure(foreground='black')
             self.which_frame = "Coloring"
+
+            # Put here the frame change
 
     def create_canvas(self):
         """
@@ -121,6 +125,7 @@ class SudokuColoringFrame(Main.StdFrame):
 
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
+
                 if matrix[i][j] != matrix[j][i]:
                     matrix[i][j] = matrix[j][i]
 
@@ -131,8 +136,12 @@ class SudokuColoringFrame(Main.StdFrame):
                     isolated_node += 1
 
             if isolated_node == len(matrix):
-                num = (np.random.randint(1, (len(matrix) / 2)))
-                for _ in range(num):
+                rep = (np.random.randint(1, len(matrix)))
+                for _ in range(rep):
+                    # try:
+                    #     matrix[i][i+1] = 1
+                    # except IndexError:
+                    #     matrix[i][i-1] = 1
                     matrix[i][np.random.randint(0, (len(matrix)))] = 1
             isolated_node = 0
             matrix[i][i] = 0
@@ -146,7 +155,7 @@ class SudokuColoringFrame(Main.StdFrame):
         for node, node_attr in self.graph.nodes(data=True):
             node_attr['size'] = 500
 
-        art = plot_network(self.graph, layout="shell", ax=self.ax,
+        art = plot_network(self.graph, layout="spring", ax=self.ax,
                            node_style=use_attributes(),
                            edge_style=use_attributes(),
                            node_label_style={'font_size': 7,
@@ -154,14 +163,6 @@ class SudokuColoringFrame(Main.StdFrame):
                                              'font_color': 'k',
                                              'bbox': {'alpha': 0}
                                              })
-
-        # Afegim per sobre el graf etiquetat de forma no interactiva.
-        edge_labels = nx.get_edge_attributes(self.graph, "weight")
-        pos = nx.shell_layout(self.graph)
-        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels, font_size=8)
-
-        # I don't know what this does
-        art.set_picker(10)
 
         self.canvas.draw()
 
