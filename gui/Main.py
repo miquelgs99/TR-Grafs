f043a9852be16ef22cdcd51473daf49a5f29e3b5
miquelgs99 <mgargallo@institutsantaeulalia.cat>
2022-09-22 21:26:49 +0200

    Make program look good

--- gui/Main.py
+++ gui/Main.py
@@ -1,39 +1,49 @@
 from tkinter import *
 from tkinter import ttk
+import customtkinter as ctk
 import MenuFrame
+
+ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
 
 class MainGui:
     def __init__(self):
-        self.root = Tk()
-        self.root.geometry('1300x900')
+        self.root = ctk.CTk()
+        self.root.geometry('1280x720')
+        self.root.resizable(False, False)
+        self.root.title("Els grafs a la vida quotidiana: problemes i algorismes amb Python")
+
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
         main_win = MenuFrame.MenuFrame()
         main_win.grid(row=0, column=0, padx=10, pady=10)
 
         # region Menubar
-
-        self.menubar = Menu(self.root)
-        self.root.config(menu=self.menubar)
-
-        self.file_menu = Menu(self.menubar, tearoff=0)
-        self.edit_menu = Menu(self.menubar, tearoff=0)
-        self.help_menu = Menu(self.menubar, tearoff=0)
-
-        self.menubar.add_cascade(label="Archivo", menu=self.file_menu)
-        self.menubar.add_cascade(label="Editar", menu=self.edit_menu)
-        self.menubar.add_cascade(label="Ayuda", menu=self.help_menu)
-
-        self.file_menu.add_command(label="´Refresh", command=self.refresh)
-
-        self.goto_menu = Menu(self.edit_menu, tearoff=0)
-        self.edit_menu.add_cascade(label="Go to", menu=self.goto_menu)
-        self.goto_menu.add_command(label='Menu', command=None)  # lambda: StdFrame.new_window(main_win, MenuFrame.MenuFrame
-        self.goto_menu.add_command(label='GraphFrame')
-        self.goto_menu.add_command(label='SudokuColoringFrame')
-
-        self.help_menu.add_command(label="GitHub")
-        self.help_menu.add_command(label="FAQ")
-        self.file_menu.add_command(label="Quit")
+        #
+        # self.menubar = Menu(self.root)
+        # self.root.config(menu=self.menubar)
+        #
+        # self.file_menu = Menu(self.menubar, tearoff=0)
+        # self.edit_menu = Menu(self.menubar, tearoff=0)
+        # self.help_menu = Menu(self.menubar, tearoff=0)
+        #
+        # self.menubar.add_cascade(label="Archivo", menu=self.file_menu)
+        # self.menubar.add_cascade(label="Editar", menu=self.edit_menu)
+        # self.menubar.add_cascade(label="Ayuda", menu=self.help_menu)
+        #
+        # self.file_menu.add_command(label="´Refresh", command=self.refresh)
+        #
+        # self.goto_menu = Menu(self.edit_menu, tearoff=0)
+        # self.edit_menu.add_cascade(label="Go to", menu=self.goto_menu)
+        # self.goto_menu.add_command(label='Menu', command=None)  # lambda: StdFrame.new_window(main_win, MenuFrame.MenuFrame
+        # self.goto_menu.add_command(label='GraphFrame')
+        # self.goto_menu.add_command(label='SudokuColoringFrame')
+        #
+        # self.help_menu.add_command(label="GitHub")
+        # self.help_menu.add_command(label="FAQ")
+        # self.file_menu.add_command(label="Quit")
         # endregion
 
     def run_gui(self):
@@ -44,7 +54,7 @@ class MainGui:
         self.__init__()
 
 
-class StdFrame(Frame):
+class StdFrame(ctk.CTkFrame):
 
     def new_window(self, win):
         self.destroy()
@@ -53,8 +63,7 @@ class StdFrame(Frame):
         win.mainloop()
 
     def __init__(self):
-        Frame.__init__(self)
-        pass
+        ctk.CTkFrame.__init__(self)
 
 
 if __name__ == '__main__':
--- gui/MenuFrame.py
+++ gui/MenuFrame.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import customtkinter as ctk
 import GraphFrame
 import SudokuColoringFrame
 import Main
@@ -10,14 +11,44 @@ class MenuFrame(Main.StdFrame):
     def __init__(self):
         Main.StdFrame.__init__(self)
 
-        # Buttons ----------
-        nav_button1 = ttk.Button(self, text="GraphFrame",
-                                 command=lambda: self.new_window(GraphFrame.GraphFrame))
-        nav_button1.grid(row=0, column=0, sticky=W)
+        self.configure(width=2000)
 
-        nav_button2 = ttk.Button(self, text="SudokuColoringFrame",
-                                 command=lambda: self.new_window(SudokuColoringFrame.SudokuColoringFrame))
-        nav_button2.grid(row=1, column=0, sticky=E)
+        # Buttons ----------
+        nav_button1 = ctk.CTkButton(self,
+                                    width=500,
+                                    height=50,
+                                    text_font=("helvetica", 14),
+                                    border_width=1,
+                                    corner_radius=8,
+                                    border_color="black",
+                                    text_color="black",
+                                    text="El problema del camí més curt",
+                                    command=lambda: self.new_window(GraphFrame.GraphFrame))
+        nav_button1.grid(row=0, column=0, padx=10, pady=10)
+
+        nav_button2 = ctk.CTkButton(self,
+                                    width=500,
+                                    height=50,
+                                    text_font=("helvetica", 14),
+                                    border_width=1,
+                                    corner_radius=8,
+                                    border_color="black",
+                                    text_color="black",
+                                    text="Coloració i resolució de sudokus",
+                                    command=lambda: self.new_window(SudokuColoringFrame.SudokuColoringFrame))
+        nav_button2.grid(row=1, column=0, padx=10, pady=10)
+
+        nav_button3 = ctk.CTkButton(self,
+                                    width=500,
+                                    height=50,
+                                    text_font=("helvetica", 14),
+                                    border_width=1,
+                                    corner_radius=8,
+                                    border_color="black",
+                                    text_color="black",
+                                    text="Optimització d'una xarxa ferroviària",
+                                    command=lambda: self.new_window(SudokuColoringFrame.SudokuColoringFrame))
+        nav_button3.grid(row=2, column=0, padx=10, pady=10)
         # -------------------
 
 
